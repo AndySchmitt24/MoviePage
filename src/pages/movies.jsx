07964bf72ace4332fdc3fd69f515f63706107759
Andy <andyschmitt24@gmail.com>
2023-09-20 13:16:22 +0200

    Show Array in Console

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,3 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Movies() {
-  return <p>Movies</p>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    console.log("This callback will be called on mount");
+    axios
+      .get(
+        "https://2zc6fti416.execute-api.eu-central-1.amazonaws.com/prod/movies/"
+      )
+      .then(({ data }) => {
+        console.log(data);
+        setMovies(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        console.log("finally");
+      });
+  }, []);
+
+  console.log("movies " + movies);
+  return (
+    <div>
+      <p>Movies</p>
+      {movies.map((movie) => {
+        console.log(movie);
+      })}
+    </div>
+  );
 }
