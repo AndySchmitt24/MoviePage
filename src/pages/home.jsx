3dac9863347a0cba8762680711c5931b48e61e8c
Andy <andyschmitt24@gmail.com>
2023-09-21 16:17:48 +0200

    Random Movie

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div></div>;
+  const [randomMovie, setRandomMovie] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        "https://2zc6fti416.execute-api.eu-central-1.amazonaws.com/prod/movies/"
+      )
+      .then(({ data }) => {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        console.log(randomIndex);
+        return axios.get(
+          "https://2zc6fti416.execute-api.eu-central-1.amazonaws.com/prod/movies/" +
+            data[randomIndex].id
+        );
+      })
+      .then(({ data }) => {
+        setRandomMovie(data);
+      })
+      .catch(() => {
+        console.log("Error");
+      })
+      .finally(() => {
+        console.log("finally");
+      });
+  }, []);
+
+  if (!randomMovie) {
+    return <div>Lade...</div>;
+  }
+  return (
+    <div>
+      <h2>Zufälliger Vorschau: {randomMovie.title}</h2>
+      <p>
+        <Link to={`movies/` + randomMovie.id}>
+          <img src={randomMovie.image} alt={randomMovie.title} />
+        </Link>
+      </p>
+    </div>
+  );
 }
